95649aed4c874de629248b4984ce89f38f9fac3d
Jaroslav Solocha <jaroslav.solocha@gmail.com>
2024-08-24 15:01:41 +0200

    add audio player

--- lib/widgets/expanded_child.dart
+++ lib/widgets/expanded_child.dart
@@ -1,6 +1,9 @@
+import 'dart:async';
+
+import 'package:audioplayers/audioplayers.dart';
 import 'package:flutter/material.dart';
 
-class ExpandedChild extends StatelessWidget {
+class ExpandedChild extends StatefulWidget {
   const ExpandedChild({
     super.key,
     required this.id,
@@ -11,17 +14,35 @@ class ExpandedChild extends StatelessWidget {
   final Color color;
 
   @override
+  State<ExpandedChild> createState() => _ExpandedChildState();
+}
+
+class _ExpandedChildState extends State<ExpandedChild> {
+  late AudioPlayer player = AudioPlayer();
+
+  @override
+  void initState() {
+    super.initState();
+
+    player = AudioPlayer();
+
+    // Set the release mode to keep the source after playback has completed.
+    player.setReleaseMode(ReleaseMode.stop);
+  }
+
+  @override
   Widget build(BuildContext context) {
     return Expanded(
       child: TextButton(
         style: TextButton.styleFrom(
           padding: EdgeInsets.zero,
         ),
-        onPressed: () {
-          print('clicked');
+        onPressed: () async {
+          await player.setSource(AssetSource('assets_note1.wav'));
+          await player.resume();
         },
         child: Container(
-          color: color,
+          color: widget.color,
         ),
       ),
     );
